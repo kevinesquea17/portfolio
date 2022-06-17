b05cccab6edeea9c030b196c05681c50989d1025
kevinesquea17 <106271406+kevinesquea17@users.noreply.github.com>
2022-06-17 11:31:06 -0500

    Update HeaderSocial.jsx

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -4,7 +4,7 @@ import {BsLinkedin, BsGithub, BsDribbble} from 'react-icons/bs'
 const HeaderSocial = () => {
   return (
     <div className="header__social">
-        <a href="https://www.linkedin.com/in/kevin-esquea-estrada-2357ab1bb/" target='_blank'><BsLinkedin /></a>
+        <a href="https://www.linkedin.com/in/kevin-esquea" target='_blank'><BsLinkedin /></a>
         <a href="https://github.com/kevinesquea17" target='_blank'><BsGithub /></a>
     </div>
   )
