2ed82156ea7e802e9a3707e9be46fca70381012f
kevinesquea17 <106271406+kevinesquea17@users.noreply.github.com>
2022-07-12 09:51:33 -0500

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './portfolio.css';
 import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/portfolio2.png'
+import IMG2 from '../../assets/codealo-ecommerce.png'
 import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
 import IMG5 from '../../assets/portfolio5.png'
